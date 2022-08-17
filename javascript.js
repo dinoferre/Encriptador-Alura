47c8830cce7beec3e61c6d99a7ca9296d59bd524
dinoferre <dinoferre@hotmail.com>
2022-08-17 14:56:29 -0300

    Update

---
+++ javascript.js
@@ -0,0 +1,40 @@
+function encriptar(){
+
+    let texto = document.getElementById("input-texto").value.toLowerCase();
+
+    let textoCifrado = texto.replace(/e/igm,"enter");
+    textoCifrado = textoCifrado.replace(/o/igm,"ober");
+    textoCifrado = textoCifrado.replace(/i/igm,"imes");
+    textoCifrado = textoCifrado.replace(/a/igm,"ai");
+    textoCifrado = textoCifrado.replace(/u/igm,"ufat");
+
+    document.querySelector(".imgDerecha").style.display = "none";
+    document.querySelector(".texto").style.display = "none";
+    document.querySelector(".textoDerecho").innerHTML = textoCifrado;
+    document.querySelector("#copiar").style.display = "show";
+    document.querySelector("#copiar").style.display = "inherit";
+}
+
+function desencriptar(){
+
+    let texto = document.getElementById("input-texto").value.toLowerCase();
+
+    let textoCifrado = texto.replace(/enter/igm,"e");
+    textoCifrado = textoCifrado.replace(/ober/igm,"o");
+    textoCifrado = textoCifrado.replace(/imes/igm,"i");
+    textoCifrado = textoCifrado.replace(/ai/igm,"a");
+    textoCifrado = textoCifrado.replace(/ufat/igm,"u");
+
+    document.querySelector(".imgDerecha").style.display = "none";
+    document.querySelector(".texto").style.display = "none";
+    document.querySelector(".textoDerecho").innerHTML = textoCifrado;
+    document.querySelector("#copiar").style.display = "show";
+    document.querySelector("#copiar").style.display = "inherit";
+}
+
+function copiar(){
+    let contenido = document.querySelector(".textoDerecho");
+    contenido.select();
+    document.execCommand("copy");
+    alert("Copiado!")
+}
